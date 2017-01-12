fda528282d7a15107087bd79cf34f69d8e777f00
Nicole <nicoleshasha@hotmail.co.uk>
2017-01-12 17:35:16 +0000

    Modify reply

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ var TwitterPackage = require('twitter');
 var secret = require("./secret");
 
 var Twitter = new TwitterPackage(secret);
-//
-// Twitter.post('statuses/update', {status: 'Testing'},  function(error, tweet, response){
-//   if(error){
-//     console.log(error);
-//   }
-//   console.log(tweet);  // Tweet body.
-//   console.log(response);  // Raw response object.
-// });
 
-Twitter.stream('statuses/filter', {track: '#TestReadBot'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '#testingbotshasha'}, function(stream) {
+
   stream.on('data', function(tweet) {
+
     console.log(tweet.text);
+
+    var statusObj = {status: "Hi @" + tweet.user.screen_name + ", thanks for programming me."}
+
+    Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
+
+      if(error){
+        console.log(error);
+      }
+
+      console.log(tweetReply.text);
+    });
   });
 
   stream.on('error', function(error) {
