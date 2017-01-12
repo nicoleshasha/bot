c12ac0af363cb7d58fcc2cfab32f5c85dbf628ba
Nicole <nicoleshasha@hotmail.co.uk>
2017-01-12 17:29:31 +0000

    Add listen function

--- app.js
+++ app.js
@@ -3,5 +3,21 @@ var TwitterPackage = require('twitter');
 var secret = require("./secret");
 
 var Twitter = new TwitterPackage(secret);
+//
+// Twitter.post('statuses/update', {status: 'Testing'},  function(error, tweet, response){
+//   if(error){
+//     console.log(error);
+//   }
+//   console.log(tweet);  // Tweet body.
+//   console.log(response);  // Raw response object.
+// });
 
-console.log("Hello World!");
+Twitter.stream('statuses/filter', {track: '#TestReadBot'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
